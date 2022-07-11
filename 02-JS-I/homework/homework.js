526d433b53310e4e5f0322eb92a59e3892427ed3
Malep2 <pedromaurileo@gmail.com>
2022-07-11 15:58:22 -0500

    SLM

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Mauri_rivas";
+const nuevaString = "Jhio_Pdro_sar_lei";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = "pedro_leopoldo";
