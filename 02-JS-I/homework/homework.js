371e5d8db1157b0a5973189f3ee617e511334264
Malep2 <pedromaurileo@gmail.com>
2022-07-11 14:54:56 -0500

    first commmit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "Hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = "pedro";
+const nuevoNum = "pedro_M";
 
 // Crea una variable booleana:
 const nuevoBool = null;
