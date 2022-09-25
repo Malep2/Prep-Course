cbf29ca1ba652c8b5a851d1bd392f4405cbcb8c1
Malep2 <pedromaurileo@gmail.com>
2022-09-25 00:00:47 -0500

    pedro

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = "Jhio_Pdro_sar_lei";
+const nuevaString = "jhio1111";
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = "pedro_leopoldo";
